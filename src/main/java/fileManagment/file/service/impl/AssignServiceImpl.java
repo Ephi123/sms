8738538f2b8ecb70ab39afedae945fe558151230
Ephi123 <ephrembereket36@gmail.com>
2024-09-23 10:47:26 +0300

    student management system

--- src/main/java/fileManagment/file/service/impl/AssignServiceImpl.java
+++ src/main/java/fileManagment/file/service/impl/AssignServiceImpl.java
@@ -1,9 +1,13 @@
 package fileManagment.file.service.impl;
 
 import fileManagment.file.entity.AssignsEntity;
+import fileManagment.file.repository.AssignTeacherRepo;
 import fileManagment.file.service.AssignService;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class AssignServiceImpl implements AssignService {
+    private final AssignTeacherRepo assignTeacherRepo;
 
     @Override
     public AssignsEntity assignTeacher() {
