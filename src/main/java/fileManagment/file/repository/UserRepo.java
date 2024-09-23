f498791823fcee19a1f20c4782f161fa0fc64582
Ephi123 <ephrembereket36@gmail.com>
2024-09-23 14:11:15 +0300

    student management system

--- src/main/java/fileManagment/file/repository/UserRepo.java
+++ src/main/java/fileManagment/file/repository/UserRepo.java
@@ -2,6 +2,8 @@ package fileManagment.file.repository;
 
 import fileManagment.file.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +14,8 @@ public interface UserRepo extends JpaRepository<UserEntity,Long> {
     Optional<UserEntity> findByEmailIgnoreCase(String email);
     Optional<UserEntity> findByUserId(String userId);
     boolean existsByEmail(String email);
-
-    Optional<List<UserEntity>> findByRoles(String role);
+   @Query("SELECT u FROM UserEntity u " +
+           "JOIN u.roles r " +
+           "WHERE r.name = :role ")
+   Optional<List<UserEntity>> findUsersByRole(@Param("role") String role);
 }
